7151f9027ee07aa9b465d6e0aa3bb59abece1009
Matthew Russell <matthew.g.russell@gmail.com>
2020-05-05 19:15:52 +0000

    Modified the install script such that it can be re-run

--- setup/install-capicxx_core_runtime-capicxx_dbus_runtime.sh
+++ setup/install-capicxx_core_runtime-capicxx_dbus_runtime.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
 
-set -e
-
-install_dir=/usr/local
-source_dir=${install_dir}/src
+declare install_dir=/usr/local
+declare source_dir=${install_dir}/src
 
 apt install -qy libgtest-dev doxygen pkg-config libcairo2-dev libexpat1
 
 echo "Installing CommonAPI"
 capi_dir="${source_dir}/capicxx-core-runtime"
-git clone https://github.com/GENIVI/capicxx-core-runtime.git "${capi_dir}" \
-  && cd "${capi_dir}"                                                      \
-  && mkdir build                                                           \
-  && cd build                                                              \
-  && cmake -D CMAKE_INSTALL_PREFIX="${install_dir}" ..                     \
-  && make                                                                  \
+if [[ ! -e "${capi_dir}" ]]; then
+  git clone https://github.com/GENIVI/capicxx-core-runtime.git "${capi_dir}"
+fi
+cd "${capi_dir}"
+git fetch origin
+git reset --hard origin/master
+if [[ ! -e build ]]; then
+  mkdir build
+fi
+cd build
+cmake -D CMAKE_INSTALL_PREFIX="${install_dir}" .. \
+  && make                                         \
   && make install
 
-dbus_dir="${source_dir}/capicxx-dbus-runtime"
+declare dbus_dir="${source_dir}/capicxx-dbus-runtime"
 VERSION=1.12.16
 echo "Patching libdbus"
 git clone https://github.com/GENIVI/capicxx-dbus-runtime.git "${dbus_dir}"
 patch_files=$(echo ${dbus_dir}/src/dbus-patches/*.patch)
 tmp="$(mktemp -d)"
 
-apt remove -qy dbus
+# apt remove -qy dbus
 wget -O "${tmp}/dbus-${VERSION}.tar.gz"                              \
   "http://dbus.freedesktop.org/releases/dbus/dbus-${VERSION}.tar.gz" \
   && tar -xzf "${tmp}/dbus-${VERSION}.tar.gz" -C "${source_dir}"     \
@@ -40,11 +44,13 @@ wget -O "${tmp}/dbus-${VERSION}.tar.gz"                              \
 
 echo "Building common-api-dbus-runtime"
 export PKG_CONFIG_PATH="${install_dir}/lib/pkgconfig:$PKG_CONFIG_PATH"
-cd "${dbus_dir}"                                                                             \
-  && mkdir build                                                                             \
-  && cd build                                                                                \
-  && cmake -D USE_INSTALLED_COMMONAPI=ON -DCMAKE_INSTALL_PREFIX=${install_dir} "${dbus_dir}" \
-  && make                                                                                    \
+cd "${dbus_dir}"
+if [[ ! -e build ]]; then
+  mkdir build
+fi
+cd build
+cmake -D USE_INSTALLED_COMMONAPI=ON -DCMAKE_INSTALL_PREFIX=${install_dir} "${dbus_dir}" \
+  && make                                                                               \
   && make install
 
 # vim: ts=2 sw=2 sts=0 expandtab ff=unix :
